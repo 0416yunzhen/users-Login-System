b1c2d13104a6389d4c8b15aefb979c0c3364f92e
0416yunzhen <eilleen10133@gmail.com>
2024-05-26 01:10:49 +0800

    加入專案檔案。

---
+++ HW-C11010327/Controllers/C11010327Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace HW_C11010327.Controllers
+{
+    public class C11010327Controller : Controller
+    {
+        // GET: C11010327
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public ActionResult member327(string myacc,string mypwd,string drone)
+        {
+            ViewBag.useracc = myacc ;
+            ViewBag.userpwd = mypwd ;
+            ViewBag.drone = drone ;
+            ViewBag.userimg = drone+".jpg";
+            return View();
+        }
+
+    }
+}
---
+++ HW-C11010327/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HW_C11010327.MvcApplication" Language="C#" %>
